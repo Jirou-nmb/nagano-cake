1d08db4cbf4f8c4cee6377e489a4db437253b41c
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2020-11-18 09:07:59 +0000

    管理者、顧客でログイン画面を分けました

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,4 @@
+class Admin::ItemsController < Admin::ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :customers
+
+  root "customers/homes#top"
+  get "/about" => "customers/homes#about"
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :items # => /admin/items etc
+  end
+
+  scope module: :customers do
+    resources :items # => /items
+  end
+
 end
