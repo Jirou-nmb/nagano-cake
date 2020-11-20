bc97dcc6b8657021050c83cc66846736ba33d039
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2020-11-20 01:33:44 +0000

    reset app

--- db/migrate/20201119235545_change_is_delated_of_customers.rb
+++
@@ -1,9 +0,0 @@
-class ChangeIsDelatedOfCustomers < ActiveRecord::Migration[5.2]
-  def up
-    change_column :customers, :is_delated, :boolean, default: false
-  end
-  
-  def down
-    change_column :customers, :is_delated, :boolean
-  end
-end
