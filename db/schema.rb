0024e87e6e5d122d1ec1f96c14e87924039e8d33
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2020-11-20 01:08:00 +0000

    管理者側の顧客管理機能を追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_070617) do
+ActiveRecord::Schema.define(version: 2020_11_19_235545) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,7 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_070617) do
     t.string "postal_code"
     t.string "address"
     t.string "phone_number"
-    t.boolean "is_delated"
+    t.boolean "is_delated", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
