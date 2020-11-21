f0c31884233d71ff74f24b04d0e718b8bd7071ec
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2020-11-21 10:36:40 +0000

    配送先管理を追加

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,24 +1,36 @@
 class Customers::AddressesController < Customers::ApplicationController
+
   def index
+    @customer = current_customer
+    @address = Address.new
+    @addresses = @customer.addresses
   end
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
-    rediect_to addresses_path
+    redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
 
   private
-  
+
   def address_params
     params.require(:address).permit(:name, :postal_code, :address)
   end
