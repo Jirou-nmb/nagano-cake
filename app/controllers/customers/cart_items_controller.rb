2adff8f04ad21c91c02d5393f55307b0997fe6b2
EC2 Default User <ec2-user@ip-172-31-84-14.ec2.internal>
2020-11-22 22:05:45 +0000

    途中

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,7 +1,20 @@
 class Customers::CartItemsController < ApplicationController
   
-  def index
-    
-    
+  def create
+    @customer = current_customer
+    @cart_item = @customer.cart_item
+    @item.customer_id = (current_customer.id)
+    if @cart_item.save
+      flash[:notice] = "カートに追加しました"
+      @cart_items = Item.all
+      redirect_to "/customers/#{@cart_items.id}"
+    else
+      
+    end
+     
   end
+  
+  def index
+  end
+
 end
