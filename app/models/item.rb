69932f97d40e35010015c47b21a564bf8f230368
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2020-11-26 05:51:39 +0000

    注文詳細への保存処理を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,15 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cat_items
   has_many :customers
+  has_many :order_details
+
   attachment :image
-  
+
   validates :genre_id, presence: true
   validates :name, presence: true, uniqueness: true
   validates :introduction, presence: true
   validates :image, presence: true
   validates :price, presence: true, numericality: true
   validates :is_active, presence: true
-  
+
 end
