3bf645be9e07b8649d35577ea8de2a1710456249
EC2 Default User <ec2-user@ip-172-31-3-249.ec2.internal>
2020-11-28 10:07:32 +0000

    ログアウト後画面遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  
+  private
+  
   def after_sign_in_path_for(resource)
     case resource
     when Admin
       admin_path
     when Customer
       items_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    else 
+      root_path
     end
   end
 
